29d3a4c8169dcd75501a1ec115f56f3fe871bdf3
Salidat71 <ssaypudinova71@gmail.com>
2024-11-08 00:47:34 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,10 @@
+naz_kursa ="Курс:'Pyton'"
+print(naz_kursa)
+wipol_zad=12
+print ("всего задач",wipol_zad)
+oph_wremj=1.5
+print("общее время выполнения",oph_wremj)
+wremj_na_od_zad=oph_wremj/wipol_zad
+print( "время на одно задание",wremj_na_od_zad)
+print(naz_kursa,"всего задач =",wipol_zad,"общее время выполнения=",oph_wremj,"время на одно задание=",wremj_na_od_zad)
+
